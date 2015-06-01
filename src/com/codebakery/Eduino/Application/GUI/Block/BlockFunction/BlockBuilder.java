bf57f17c19e5e33c7d8b0523ec4a66b8d1b54284
codertimo <codertimo@gmail.com>
2015-06-01 17:46:14 +0900

    First Checking Code //Not Running Yet

--- src/com/codebakery/Eduino/Application/GUI/Block/BlockFunction/BlockBuilder.java
+++ src/com/codebakery/Eduino/Application/GUI/Block/BlockFunction/BlockBuilder.java
@@ -9,9 +9,18 @@ import com.codebakery.Eduino.Application.Main.Main;
  * Created by codertimo on 15. 5. 29..
  */
 public class BlockBuilder {
+    public BlockBuilder (Block block)
+    {
+        if(block.id.blockGroup!=null)
+            updateUI(block);
+
+        //else blockgroup is null
+        //No Reason to Update UI
+    }
     public BlockBuilder(Range result,Block block){
+
         BlockGroup dstBlockGroup = new BlockGroup();
-        if(result.isUp)
+        if(result.isUp)  //Result is UP
         {
             if(block.id.blockGroup!=null)
                 dstBlockGroup.addAll(block.id.blockGroup);
@@ -40,22 +49,59 @@ public class BlockBuilder {
         Main.controlCenter.updateBlockList(dstBlockGroup);
         //Updtae BlockList
 
-        updateUI(result,block);
+        updateUI(result, block);
     }
-    private void updateUI(Range result,Block block)
+    private void updateUI(Block block)  // No Result
+    {
+        double height_sum =0;
+        for(int i=1;i<block.id.blockGroup.size();i++)
+            height_sum+=block.id.blockGroup.get(i).getHeight();
+        //Get total sum
+
+        double block_height = block.getHeight();
+        for(int i=1;i<block.id.blockGroup.size();i++)
+        {
+            Block thisblock = block.id.blockGroup.get(i);
+
+            Main.root.getChildren().add(thisblock);  //1. Main root에 Node를 추가
+            thisblock.setLayoutX(block.getLayoutX());
+            thisblock.setLayoutY(block.getLayoutY()+(block_height-height_sum));  //2. X,Y좌표값을 설정
+
+            height_sum -= thisblock.getHeight();
+        }
+        //setPosition of Block
+    }
+
+    private void updateUI(Range result,Block block)  //Exist Result
     {
         Block standard  = result.block;
+        //Result 내의 Block 가져옴 <- 클릭한 블럭 위 또는 아래 블럭
 
-        for(int i=standard.id.blockGroup.indexOf(standard)+1;i<standard.id.blockGroup.size();i++)
+        double height_sum = 0;
+        for(int i=block.id.blockGroup.indexOf(block)+1;i<block.id.blockGroup.size();i++)
         {
-            Block temp = standard.id.blockGroup.get(i);
-
-            temp.setLayoutX(standard.getLayoutX());
-            temp.setLayoutY(standard.id.blockGroup.get(i-1).getLayoutY()+  //이전 블럭의 Y값을 가져옴
-                            standard.id.blockGroup.get(i-1).getHeight());  //이전 블럭의 Height값을 더함
+            Block temp = block.id.blockGroup.get(i);
+            height_sum+=temp.getHeight();
         }
+        //Click 한 블럭의 VBox Height를 가져옴
 
-            ///Start From Here about UI
+        block.setLayoutX(standard.getLayoutX());
+        block.setLayoutY(standard.getLayoutY()+standard.getHeight());
+        //Vbox 전체를 result로 이동시킴
+
+        double block_height = block.getHeight();
+        for(int i=standard.id.blockGroup.indexOf(standard)+1;i<standard.id.blockGroup.size();i++)  //
+        {
+            Block thisBlock = standard.id.blockGroup.get(i);
+
+            Main.root.getChildren().add(thisBlock);
+
+            thisBlock.setLayoutX(block.getLayoutX());
+            thisBlock.setLayoutY(block.getLayoutY()+(block_height-height_sum));
+
+            height_sum-=thisBlock.getHeight();
+        }
+        ///Start From Here about UI
 
     }
 }
